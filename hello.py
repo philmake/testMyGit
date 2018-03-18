8be4a3e03bcda1b68eca61ee17f1d6110ead9473
Filip <philmake@gmail.com>
2018-03-18 23:01:32 +0100

    Just testing

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print("I'm adding new text and testing branching function!")
 x = 20
 y = 60
 print("Result: {}".format(x+y))
+print("Hello, this is my python test for GitHub")
+print("I'm adding new text and testing branching function!")
